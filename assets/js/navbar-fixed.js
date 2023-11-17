345d3d05a9b9200fe472a37e50f9fea28beb2bb3
Sofia Acevedo <sofiacandelaria1992@gmail.com>
2023-11-17 16:56:09 +0000

    navbar-fixed

--- assets/js/navbar-fixed.js
+++ assets/js/navbar-fixed.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let navbar = document.querySelector(".menu__navbar");
 
     window.addEventListener("scroll", ()=> {
-        
-    })
+        let scroll = window.screenY;
 
-}
+        if(scroll =120){
+            navbar.classList.add("menu__navbar--fixed");
+        }else{
+            navbar.classList.remove("menu__navbar--fixed");
+        }
+    });
+
+};
